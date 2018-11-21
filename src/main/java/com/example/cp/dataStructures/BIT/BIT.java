6b76ffb17efc1817de2ce64dc7c323abed5642ad
saurabh <SaurabhKumar.Prajapati@cdk.com>
2018-11-21 13:16:06 +0530

    Updating BIT for range Update

--- src/main/java/com/example/cp/dataStructures/BIT/BIT.java
+++ src/main/java/com/example/cp/dataStructures/BIT/BIT.java
@@ -1,4 +1,23 @@
 package com.example.cp.dataStructures.BIT;
+
+class BITUtil{
+    void updateRange(BIT bit1, BIT bit2, long val,int l, int r) {
+
+        bit1.update(l, val);
+        bit1.update(r+1, -val);
+
+        bit2.update(l, val*(l-1));
+        bit2.update(r+1, -val*r);
+    }
+
+    long rangeSum(BIT bit1, BIT bit2, int l, int r) {
+        return sum(r, bit1, bit2) - sum(l-1, bit1, bit2);
+    }
+
+    private long sum(int x, BIT bit1, BIT bit2) {
+        return bit1.read(x)*x - bit2.read(x);
+    }
+}
 class BIT {
     
     private long tree[];
